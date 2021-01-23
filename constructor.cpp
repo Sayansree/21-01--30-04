618c3cc6c98d406608ed8da104a712c1e9f1173a
Sayansree <sayansreeparia@gmail.com>
2021-01-23 20:58:23 +0530

    constructor
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

---
+++ constructor.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+        A(int x,int y){
+            this->x=x;
+            this->y=y;
+            cout<<"constructor called"<<endl;
+        }
+        A(int x,int y,int z):x(x*z),y(y*z){
+            cout<<"constructor called"<<endl;
+        }
+        ~A(){
+            cout<<"destructor called"<<endl;
+        }
+        int x,y;
+        
+    
+
+};
+int main ()
+{
+    A obj(1,2,3), obj2(1,2);
+    cout<<obj.x<<obj.y<<obj2.x<<obj2.y<<endl;
+    
+    return 0;
+}
