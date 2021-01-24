ede40c2e616ee095fbf2a77b0edb9ad14265b9ed
Sayansree <sayansreeparia@gmail.com>
2021-01-24 21:25:09 +0530

    constructor
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- constructor.cpp
+++ constructor.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 class A{
     public:
-        A(int x,int y){
+        A(int x,int y){                     //parameterised 
             this->x=x;
             this->y=y;
             cout<<"constructor called"<<endl;
         }
-        A(int x,int y,int z):x(x*z),y(y*z){
+
+        A(int x,int y,int z):x(x*z),y(y*z){ //initialiser list
             cout<<"constructor called"<<endl;
         }
+
+        A(){                                //default constructor 
         
+        }
+    
         int x,y;
         
     
@@ -18,8 +23,9 @@ class A{
 };
 int main ()
 {
-    A obj(1,2,3), obj2(1,2);
-    cout<<obj.x<<obj.y<<obj2.x<<obj2.y<<endl;
-    
+    A obj(1,2,3), obj2(1,2), obj1;
+    cout<<obj.x<<'\t'<<obj.y<<endl;
+    cout<<obj2.x<<'\t'<<obj2.y<<endl;
+    cout<<obj1.x<<'\t'<<obj1.y<<endl;//garbage value
     return 0;
 }
